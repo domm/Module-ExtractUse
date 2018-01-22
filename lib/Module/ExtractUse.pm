b6129337db6510aaabbcbeb556f5e5c8382dfd83
Thomas Klausner <domm@plix.at>
2018-01-22 16:18:55 +0100

    bump version

--- lib/Module/ExtractUse.pm
+++ lib/Module/ExtractUse.pm
@@ -8,7 +8,7 @@ use Pod::Strip;
 use Parse::RecDescent 1.967009;
 use Module::ExtractUse::Grammar;
 use Carp;
-our $VERSION = '0.341';
+our $VERSION = '0.342';
 
 # ABSTRACT: Find out what modules are used
 
